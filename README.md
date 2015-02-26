7f7f9408a7a516dd2a22d5372155d3627c6a95db
Michael Brown <mike.brown@allianz.com.au>
2015-02-26 15:35:01 +1100

    Start.  Working, but on when changing the axes on the bootstrap version

---
+++ README.md
@@ -0,0 +1,3 @@
+# react.slider.test
+
+A ReactJS wrapper for the HTML5 input type="range" slider component, but using seiyria's Bootstrap Slider component -https://github.com/seiyria/bootstrap-slider - as a polyfill where the HTML5 version is not available (IE 9).
---
+++ javascript/src/app.jsx
@@ -0,0 +1,130 @@
+/** @jsx React.DOM */
+/* jshint browser: true */
+/* globals React, ReactSlider, console */
+
+
+var SliderNative = React.createClass({
+    render: function () {
+        return (
+            <input id="mySlider"
+                type="range"
+                value={this.props.value}
+                min={this.props.min}
+                max={this.props.max}
+                onChange={this.props.handleChange}
+                step={this.props.step} />
+        );
+    }
+});
+
+var BootstrapSlider = React.createClass({
+    render: function () {
+        return (
+                <input id="mySlider"
+                    type="range"
+                    data-slider-value={this.props.value}
+                    data-slider-min={this.props.min}
+                    data-slider-max={this.props.max}
+                    data-slider-step={this.props.step} />
+            );
+    },
+    componentDidMount: function () {
+        var that = this;
+        var mySlider = $(this.getDOMNode()).slider().on("change", function (e) {
+            var fakeEvent = {
+                target: {}
+            };
+            fakeEvent.target.value = e.value.newValue;
+            that.props.handleChange(fakeEvent);
+        });
+    }
+});
+
+
+var SliderNativeBootstrap = React.createClass({
+    componentWillMount: function () {
+        // Test whether range input is accepted by creating one, then seeing what its
+        // type is set to.  
+        console.log("Writing supportsRange()");
+        var input = document.createElement('input');
+        input.setAttribute('type', 'range');
+        this.supportsRange = input.type !== "text" ? true : false;
+    },
+    render: function () {
+        var polyfill = typeof this.props.polyfill == "undefined" ? true : this.props.polyfill;
+        if(polyfill) {
+            if(this.supportsRange) {
+                return (
+                    <SliderNative {...this.props} />
+                );
+            }
+            else {
+                return (
+                    <BootstrapSlider {...this.props} />
+                );
+            }
+        }
+        else {
+            return (
+                <BootstrapSlider {...this.props} />
+            );            
+        }
+    }
+});
+
+
+
+
+/**********************************************************************************/
+
+
+var Demo = React.createClass({
+    getInitialState: function (){
+        return {
+            currentValue: this.props.startValue,
+            min: this.props.min,
+            max: this.props.max,
+            step: this.props.step
+        };
+    },
+    render: function() {
+        console.log("Demo.render(). this.state.currentValue = " + this.state.currentValue);
+        var newValue = this.state.currentValue;
+        // TODO: Replace this with bootstrap version
+        return (
+            <div>
+                <SliderNativeBootstrap
+                    polyfill={false}
+                    value={this.state.currentValue}
+                    handleChange={this.changeValue}
+                    step={this.state.step}
+                    max={this.state.max}
+                    min={this.state.min} />
+                <br /><br />
+                Value: {newValue}
+
+                <br /><br />
+                <button onClick={this.changeAll}>Change all!</button>
+            </div>
+            );
+    },
+    changeValue: function(e) {
+        console.log("value = " + e.target.value);
+        this.setState({currentValue: e.target.value});
+    },
+    changeAll: function (){
+        this.setState({
+            currentValue: 500,
+            min: 0,
+            max: 2000,
+            step: 100
+        });
+    }
+});
+
+
+React.render(<Demo
+        startValue={3000}
+        max={20000}
+        min={1000}
+        step={1000} />, document.getElementById("main"));
