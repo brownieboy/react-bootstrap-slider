46832c533398f0f1f137c5749a27d7381d9f0a83
Michael Brown <brownieboy@gmail.com>
2016-02-21 12:19:58 +1100

    Framework now transpiling via Babel & Webpack , but code is still all empty shells

---
+++ src/js/app.jsx
@@ -0,0 +1,5 @@
+import reactNativeBootstrapSlider from  './react-native-bootstrap-slider.jsx';
+console.log("app.jsx loaded");
+
+const mySlider = new reactNativeBootstrapSlider();
+
---
+++ src/js/react-native-bootstrap-slider.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log("react-native-bootstrapslider.jsx loaded");
+
+class reactNativeBootstrapSlider {
+	constructor() {
+		console.log("reactNativeBootstrapSlider constructor ran...");
+	}
+}
+
+export default reactNativeBootstrapSlider;
