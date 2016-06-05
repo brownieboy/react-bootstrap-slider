f6b100b5bb686601a295049d07355cfbba09663a
Michael Brown <brownieboy@gmail.com>
2016-06-05 16:06:55 +1000

    Added babel-preset-stage-2 for object literal spread operator

--- src/js/react-bootstrap-slider.jsx
+++ src/js/react-bootstrap-slider.jsx
@@ -6,58 +6,65 @@ import Slider from "bootstrap-slider";
 import es6BindAll from "es6bindall";
 
 export class ReactBootstrapSlider extends React.Component {
-    constructor(props) {
-        super(props);
-        es6BindAll(this, ["updateSliderValues"]);
-    }
-    render() {
-        // The slider"s an input.  That"s all we need.  We"ll do the rest in
-        // the componentDidMount() method.
-        return <input / >;
-    }
+   constructor(props) {
+      super(props);
+      es6BindAll(this, ["updateSliderValues"]);
+   }
+   render() {
+      // The slider"s an input.  That"s all we need.  We"ll do the rest in
+      // the componentDidMount() method.
+      return <input / >;
+   }
 
-    componentDidMount() {
-        var that = this;
-        var sliderAttributes = {
-            "tooltip": this.props.tooltip || "show"
-        };
-        if (this.props.orientation) {
-            console.log("orientation = " + this.props.orientation);
-            sliderAttributes.orientation = this.props.orientation;
-        }
+   componentDidMount() {
+      var that = this;
+      // var sliderAttributes = {
+      //     "tooltip": this.props.tooltip || "show"
+      // };
+      // if (this.props.orientation) {
+      //     sliderAttributes.orientation = this.props.orientation;
+      // }
+      var sliderAttributes = {
+         ...this.props,
+         "tooltip": this.props.tooltip || "show"
+      };
+      // if (this.props.orientation) {
+      //    sliderAttributes.orientation = this.props.orientation;
+      // }
 
-        this.mySlider = new Slider(ReactDOM.findDOMNode(this), sliderAttributes);
 
-        this.updateSliderValues();
-        this.mySlider.on("change", function(e) {
-            var fakeEvent = {
-                target: {}
-            };
-            fakeEvent.target.value = e.newValue;
-            that.props.handleChange(fakeEvent);
-        });
-    }
-    componentDidUpdate() {
-        this.updateSliderValues();
-    }
-    updateSliderValues() {
-        this.mySlider.setAttribute("min", this.props.min);
-        this.mySlider.setAttribute("max", this.props.max);
-        this.mySlider.setAttribute("step", this.props.step);
-        this.mySlider.setValue(this.props.value);
+      this.mySlider = new Slider(ReactDOM.findDOMNode(this), sliderAttributes);
 
-        var sliderEnable = this.props.disabled === "disabled" ? false : true;
-        var currentlyEnabled = this.mySlider.isEnabled();
-        if (sliderEnable) {
-            if (!currentlyEnabled) {
-                this.mySlider.enable();
-            }
-        } else {
-            if (currentlyEnabled) {
-                this.mySlider.disable();
-            }
-        }
-    }
+      this.updateSliderValues();
+      this.mySlider.on("change", function(e) {
+         var fakeEvent = {
+            target: {}
+         };
+         fakeEvent.target.value = e.newValue;
+         that.props.handleChange(fakeEvent);
+      });
+   }
+   componentDidUpdate() {
+      this.updateSliderValues();
+   }
+   updateSliderValues() {
+      this.mySlider.setAttribute("min", this.props.min);
+      this.mySlider.setAttribute("max", this.props.max);
+      this.mySlider.setAttribute("step", this.props.step);
+      this.mySlider.setValue(this.props.value);
+
+      var sliderEnable = this.props.disabled === "disabled" ? false : true;
+      var currentlyEnabled = this.mySlider.isEnabled();
+      if (sliderEnable) {
+         if (!currentlyEnabled) {
+            this.mySlider.enable();
+         }
+      } else {
+         if (currentlyEnabled) {
+            this.mySlider.disable();
+         }
+      }
+   }
 }
 
 export default ReactBootstrapSlider;
