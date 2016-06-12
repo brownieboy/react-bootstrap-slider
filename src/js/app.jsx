cf1a53b6579a222eb12e6c347db914c86b8e9f5c
Michael Brown <brownieboy@gmail.com>
2016-06-12 21:54:28 +1000

    Removed three different versions of Demo class.  Now have only one and vary it with props and sub-components

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -6,161 +6,121 @@ import es6BindAll from "es6bindall";
 import ReactBootstrapSlider from "./react-bootstrap-slider.jsx";
 
 const wrapperDivStyles = {
-    "backgroundColor": "#E0E0E0",
-    "padding": "20px",
-    "width": "300px"
+   "backgroundColor": "#E0E0E0",
+   "padding": "20px",
+   "width": "300px"
 };
 
+class DemoSingleValueSpan extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      var value = this.props.value;
+      var id = this.props.id;
+      return (
+         <span>
+            Value: <span id={"valueSpan" + id}>{ value }</span>
+        </span>
+      );
+   }
+}
+
+class DemoMultiValueSpan extends React.Component {
+   constructor(props) {
+      super(props);
+   }
+
+   render() {
+      var value = this.props.value;
+      var id = this.props.id;
+      return (
+         <span>
+            Lower Value: <span id={"valueSpan" + id + "Low"}>{ value[0] }</span>
+            Upper Value: <span id={"valueSpan" + id + "High"}>{ value[1] }</span>
+        </span>
+      );
+   }
+}
+
 class Demo extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...this.props,
-            currentValue: props.startValue
-        };
-        delete this.state.startValue;
-        es6BindAll(this, ["changeValue", "changeAxes"]);
-    }
-    changeValue(e) {
-        this.setState({ currentValue: e.target.value });
-    }
-    changeAxes() {
-        this.setState({
-            currentValue: 500,
-            min: 0,
-            max: 2000,
-            step: 100
-        });
-    }
-    render() {
-        var newValue = this.state.currentValue;
-        return (
-            <div>
-                <div style={wrapperDivStyles}>
-                   <ReactBootstrapSlider
-                        { ...this.state }
-                        id="horizontalSlider"
-                        value = { this.state.currentValue }
-                        handleChange = { this.changeValue } />
-                </div>
-                 <br /> <br />
-                Value: <span id="valueSpanHorizontal">{ newValue }</span>
-                <br /><br />
-                <button id = "butHorizontal" onClick = { this.changeAxes } > Change axes </button>
+   constructor(props) {
+      super(props);
+      this.state = {
+         ...this.props,
+         currentValue: props.startValue
+      };
+      delete this.state.startValue;
+      es6BindAll(this, ["changeValue", "changeAxes"]);
+   }
+   changeValue(e) {
+      this.setState({ currentValue: e.target.value });
+   }
+   changeAxes() {
+      this.setState({
+         currentValue: 500,
+         min: 0,
+         max: 2000,
+         step: 100
+      });
+   }
+   render() {
+      var newValue = this.state.currentValue;
+      var id = this.props.id;
+      var valueSpan;
+      if (Array.isArray(newValue)) {
+         valueSpan = (<DemoMultiValueSpan
+                    id = { id }
+                    value = { newValue } />);
+      } else {
+         valueSpan = <DemoSingleValueSpan
+            id = { id }
+            value = { newValue } />;
+      }
+      return (
+         <div>
+            <div style={wrapperDivStyles}>
+               <ReactBootstrapSlider
+                    { ...this.state }
+                    value = { this.state.currentValue }
+                    handleChange = { this.changeValue } />
             </div>
-        );
-    }
-
-}
-
-class DemoVertical extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...this.props,
-            currentValue: props.startValue
-        };
-        delete this.state.startValue;
-        es6BindAll(this, ["changeValue", "changeAxes"]);
-    }
-    changeValue(e) {
-        this.setState({ currentValue: e.target.value });
-    }
-    changeAxes() {
-        this.setState({
-            currentValue: 700,
-            min: 0,
-            max: 2500,
-            step: 500
-        });
-    }
-    render() {
-        var newValue = this.state.currentValue;
-        return (
-            <div>
-                <div style={wrapperDivStyles}>
-                   <ReactBootstrapSlider
-                        { ...this.state }
-                        id="verticalSlider"
-                        value = { this.state.currentValue }
-                        handleChange = { this.changeValue }
-                        orientation = "vertical" />
-                </div>
-                 <br /> <br />
-                Value: <span id="valueSpanVertical">{ newValue }</span>
-                <br /><br />
-                <button id="butVertical" onClick = { this.changeAxes } > Change axes </button>
-            </div>
-        );
-    }
-
-}
-
-class DemoDual extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...this.props,
-            currentValue: props.startValue
-        };
-        delete this.state.startValue;
-        es6BindAll(this, ["changeValue", "changeAxes"]);
-    }
-    changeValue(e) {
-        this.setState({ currentValue: e.target.value });
-    }
-    changeAxes() {
-        this.setState({
-            currentValue: 500,
-            min: 0,
-            max: 2000,
-            step: 100
-        });
-    }
-    render() {
-        var newValue = this.state.currentValue;
-        return (
-            <div>
-                <div style={wrapperDivStyles}>
-                   <ReactBootstrapSlider
-                        { ...this.state }
-                        id="dualSlider"
-                        range={ true }
-                        value = { this.state.currentValue }
-                        handleChange = { this.changeValue } />
-                </div>
-                 <br /> <br />
-                Lower Value: <span id="valueSpanHorizontalLow">{ newValue[0] }</span><br />
-                Upper Value: <span id="valueSpanHorizontalHigh">{ newValue[1] }</span>
-                <br /><br />
-
-                <button id = "butHorizontal" onClick = { this.changeAxes } > Change axes </button>
-            </div>
-        );
-    }
+             <br /> <br />
+             { valueSpan }
+            <br /><br />
+            <button id = {"but" + id} onClick = { this.changeAxes } > Change axes </button>
+        </div>
+      );
+   }
 
 }
 
 ReactDOM.render(
-    <div>
+   <div>
         <h3>Horizontal (default) demo</h3>
-        <Demo startValue = { 3000 }
+        <Demo
+            id = "horizontalSlider"
+            startValue = { 3000 }
             max = { 20000 }
             min = { 1000 }
             step = { 1000 } />
 
         <h3>Vertical Demo</h3>
-        <DemoVertical startValue = { 3000 }
+        <Demo startValue = { 3000 }
+            id = "verticalSlider"
+            orientation = "vertical"
             max = { 20000 }
             min = { 1000 }
             step = { 1000 }
             reversed = { true } />
 
         <h3>Dual demo</h3>
-        <DemoDual startValue = { [3000, 10000] }
+        <Demo startValue = { [3000, 10000] }
+            range = { true }
+            id = "dualSlider"
             max = { 20000 }
             min = { 1000 }
             step = { 1000 } />
 
     </div>, document.getElementById("main"));
-
--- tests/e2e/slidertest1.js
+++ tests/e2e/slidertest1.js
@@ -11,11 +11,11 @@ describe("Page loads", function() {
 
       browser.wait(EC.presenceOf(element(by.css("body")), 1000));
       browser.wait(EC.presenceOf(element(by.id("valueSpanHorizontal")), 2000));
-      expect(element(by.id("valueSpanHorizontal")).getText()).toEqual("3000");
+      expect(element(by.id("valueSpanhorizontalSlider")).getText()).toEqual("3000");
    });
 
    it("should display vertical Demo value of '3000'", function() {
-      expect(element(by.id("valueSpanVertical")).getText()).toEqual("3000");
+      expect(element(by.id("valueSpanverticalSlider")).getText()).toEqual("3000");
    });
 });
 
@@ -26,7 +26,7 @@ describe("Dragging horizontal slider 100px to the right", function() {
       browser.actions().dragAndDrop(
          slider, { x: 100, y: 0 }
       ).perform();
-      expect(element(by.id("valueSpanHorizontal")).getText()).toEqual("12000");
+      expect(element(by.id("valueSpanhorizontalSlider")).getText()).toEqual("12000");
    });
 });
 
@@ -38,20 +38,22 @@ describe("Dragging vertical slider 50px upwards", function() {
       browser.actions().dragAndDrop(
          slider, { x: 0, y: -50 }
       ).perform();
-      expect(element(by.id("valueSpanVertical")).getText()).toEqual("8000");
+      expect(element(by.id("valueSpanverticalSlider")).getText()).toEqual("8000");
    });
 });
 
 describe("Clicking Change Axes button for horizontal demo", function() {
    it("should display horizontal Demo value of '500'", function() {
-      element(by.id("butHorizontal")).click();
-      expect(element(by.id("valueSpanHorizontal")).getText()).toEqual("500");
+      element(by.id("buthorizontalSlider")).click();
+      expect(element(by.id("valueSpanhorizontalSlider")).getText()).toEqual("500");
    });
 });
 
 describe("Clicking Change Axes button for vertical demo", function() {
    it("should display horizontal Demo value of '700'", function() {
-      element(by.id("butVertical")).click();
-      expect(element(by.id("valueSpanVertical")).getText()).toEqual("700");
+      element(by.id("butverticalSlider")).click();
+      expect(element(by.id("valueSpanverticalSlider")).getText()).toEqual("700");
    });
 });
+
+
