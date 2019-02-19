06dc40eb767f02d916be251e0ba81c45a3f19373
Michael Brown <brownieboy@gmail.com>
2019-02-19 19:40:33 +1100

    Spliting demo files off to a examples folder...

--- dist/react-bootstrap-slider.js
+++ dist/react-bootstrap-slider.js
@@ -1,32 +1,51 @@
-import _objectSpread from "/home/brownieboy/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/objectSpread";
-import _classCallCheck from "/home/brownieboy/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/classCallCheck";
-import _createClass from "/home/brownieboy/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/createClass";
-import _possibleConstructorReturn from "/home/brownieboy/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn";
-import _getPrototypeOf from "/home/brownieboy/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/getPrototypeOf";
-import _inherits from "/home/brownieboy/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/inherits";
+"use strict";
+
+var _interopRequireDefault = require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = exports.ReactBootstrapSlider = void 0;
+
+var _objectSpread2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/objectSpread"));
+
+var _classCallCheck2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/classCallCheck"));
+
+var _createClass2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/createClass"));
+
+var _possibleConstructorReturn2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn"));
+
+var _getPrototypeOf3 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/getPrototypeOf"));
+
+var _assertThisInitialized2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/assertThisInitialized"));
+
+var _inherits2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/inherits"));
+
+var _defineProperty2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/defineProperty"));
+
+var _react = _interopRequireDefault(require("react"));
+
+var _bootstrapSlider = _interopRequireDefault(require("bootstrap-slider"));
 
 /* eslint-env browser */
-import React from "react";
-import Slider from "bootstrap-slider";
-export var ReactBootstrapSlider =
+var ReactBootstrapSlider =
 /*#__PURE__*/
 function (_React$Component) {
-  _inherits(ReactBootstrapSlider, _React$Component);
+  (0, _inherits2.default)(ReactBootstrapSlider, _React$Component);
 
   function ReactBootstrapSlider() {
     var _getPrototypeOf2;
 
     var _this;
 
-    _classCallCheck(this, ReactBootstrapSlider);
+    (0, _classCallCheck2.default)(this, ReactBootstrapSlider);
 
     for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
       args[_key] = arguments[_key];
     }
 
-    _this = _possibleConstructorReturn(this, (_getPrototypeOf2 = _getPrototypeOf(ReactBootstrapSlider)).call.apply(_getPrototypeOf2, [this].concat(args)));
-
-    _this.checkAndDoDisabled = function () {
+    _this = (0, _possibleConstructorReturn2.default)(this, (_getPrototypeOf2 = (0, _getPrototypeOf3.default)(ReactBootstrapSlider)).call.apply(_getPrototypeOf2, [this].concat(args)));
+    (0, _defineProperty2.default)((0, _assertThisInitialized2.default)(_this), "checkAndDoDisabled", function () {
       var sliderEnable = _this.props.disabled !== "disabled";
 
       var currentlyEnabled = _this.mySlider.isEnabled();
@@ -40,9 +59,8 @@ function (_React$Component) {
           _this.mySlider.disable();
         }
       }
-    };
-
-    _this.updateSliderValues = function () {
+    });
+    (0, _defineProperty2.default)((0, _assertThisInitialized2.default)(_this), "updateSliderValues", function () {
       if (typeof _this.props.min !== "undefined" && (typeof _this.mySlider.min !== "undefined" || typeof _this.mySlider.options.min !== "undefined")) {
         _this.mySlider.setAttribute("min", _this.props.min);
       }
@@ -58,22 +76,19 @@ function (_React$Component) {
       _this.mySlider.setValue(_this.props.value);
 
       _this.checkAndDoDisabled();
-    };
-
+    });
     return _this;
   }
 
-  _createClass(ReactBootstrapSlider, [{
+  (0, _createClass2.default)(ReactBootstrapSlider, [{
     key: "componentDidMount",
     value: function componentDidMount() {
       var that = this;
-
-      var sliderAttributes = _objectSpread({}, this.props, {
+      var sliderAttributes = (0, _objectSpread2.default)({}, this.props, {
         tooltip: this.props.tooltip || "show"
       }); // console.log("sliderAttributes = " + JSON.stringify(sliderAttributes, null, 4));
 
-
-      this.mySlider = new Slider(this.node, sliderAttributes); //     this.updateSliderValues();
+      this.mySlider = new _bootstrapSlider.default(this.node, sliderAttributes); //     this.updateSliderValues();
 
       if (this.props.change || this.props.handleChange) {
         var changeEvent = this.props.change || this.props.handleChange;
@@ -117,14 +132,16 @@ function (_React$Component) {
 
       // The slider"s an input.  That"s all we need.  We"ll do the rest in
       // the componentDidMount() method.
-      return React.createElement("div", {
+      return _react.default.createElement("div", {
         ref: function ref(node) {
           return _this2.node = node;
         }
       });
     }
   }]);
-
   return ReactBootstrapSlider;
-}(React.Component);
-export default ReactBootstrapSlider;
+}(_react.default.Component);
+
+exports.ReactBootstrapSlider = ReactBootstrapSlider;
+var _default = ReactBootstrapSlider;
+exports.default = _default;
