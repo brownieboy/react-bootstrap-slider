ff201116237b01c17f24941559f88e824fe1cb18
Michael Brown <brownieboy@gmail.com>
2019-02-25 02:09:44 +0000

    Fixed absolute require path issue.  Replaced @babel/react-app preset with @babel/react.

--- dist/react-bootstrap-slider.js
+++ dist/react-bootstrap-slider.js
@@ -1,99 +1,79 @@
-"use strict";
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports", "react", "prop-types", "bootstrap-slider"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports, require("react"), require("prop-types"), require("bootstrap-slider"));
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports, global.react, global.propTypes, global.bootstrapSlider);
+    global.reactBootstrapSlider = mod.exports;
+  }
+})(this, function (_exports, _react, _propTypes, _bootstrapSlider) {
+  "use strict";
 
-var _interopRequireDefault = require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/interopRequireDefault");
+  Object.defineProperty(_exports, "__esModule", {
+    value: true
+  });
+  _exports.default = _exports.ReactBootstrapSlider = void 0;
+  _react = _interopRequireDefault(_react);
+  _propTypes = _interopRequireDefault(_propTypes);
+  _bootstrapSlider = _interopRequireDefault(_bootstrapSlider);
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.default = exports.ReactBootstrapSlider = void 0;
+  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-var _objectSpread2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/objectSpread"));
+  function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
-var _classCallCheck2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/classCallCheck"));
+  class ReactBootstrapSlider extends _react.default.Component {
+    constructor(...args) {
+      super(...args);
 
-var _createClass2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/createClass"));
+      _defineProperty(this, "checkAndDoDisabled", () => {
+        const sliderEnable = this.props.disabled !== "disabled";
+        const currentlyEnabled = this.mySlider.isEnabled();
 
-var _possibleConstructorReturn2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/possibleConstructorReturn"));
+        if (sliderEnable) {
+          if (!currentlyEnabled) {
+            this.mySlider.enable();
+          }
+        } else {
+          if (currentlyEnabled) {
+            this.mySlider.disable();
+          }
+        }
+      });
 
-var _getPrototypeOf3 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/getPrototypeOf"));
+      _defineProperty(this, "updateSliderValues", () => {
+        if (typeof this.props.min !== "undefined" && (typeof this.mySlider.min !== "undefined" || typeof this.mySlider.options.min !== "undefined")) {
+          this.mySlider.setAttribute("min", this.props.min);
+        }
 
-var _assertThisInitialized2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/assertThisInitialized"));
+        if (typeof this.props.max !== "undefined" && (typeof this.mySlider.max !== "undefined" || typeof this.mySlider.options.max !== "undefined")) {
+          this.mySlider.setAttribute("max", this.props.max);
+        }
 
-var _inherits2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/inherits"));
+        if (typeof this.props.step !== "undefined" && (typeof this.mySlider.step !== "undefined" || typeof this.mySlider.options.step !== "undefined")) {
+          this.mySlider.setAttribute("step", this.props.step);
+        }
 
-var _defineProperty2 = _interopRequireDefault(require("/Users/michaelbrown/Development/react-bootstrap-slider/node_modules/@babel/runtime/helpers/esm/defineProperty"));
-
-var _react = _interopRequireDefault(require("react"));
-
-var _bootstrapSlider = _interopRequireDefault(require("bootstrap-slider"));
-
-/* eslint-env browser */
-var ReactBootstrapSlider =
-/*#__PURE__*/
-function (_React$Component) {
-  (0, _inherits2.default)(ReactBootstrapSlider, _React$Component);
-
-  function ReactBootstrapSlider() {
-    var _getPrototypeOf2;
-
-    var _this;
-
-    (0, _classCallCheck2.default)(this, ReactBootstrapSlider);
-
-    for (var _len = arguments.length, args = new Array(_len), _key = 0; _key < _len; _key++) {
-      args[_key] = arguments[_key];
+        this.mySlider.setValue(this.props.value);
+        this.checkAndDoDisabled();
+      });
     }
 
-    _this = (0, _possibleConstructorReturn2.default)(this, (_getPrototypeOf2 = (0, _getPrototypeOf3.default)(ReactBootstrapSlider)).call.apply(_getPrototypeOf2, [this].concat(args)));
-    (0, _defineProperty2.default)((0, _assertThisInitialized2.default)(_this), "checkAndDoDisabled", function () {
-      var sliderEnable = _this.props.disabled !== "disabled";
-
-      var currentlyEnabled = _this.mySlider.isEnabled();
-
-      if (sliderEnable) {
-        if (!currentlyEnabled) {
-          _this.mySlider.enable();
-        }
-      } else {
-        if (currentlyEnabled) {
-          _this.mySlider.disable();
-        }
-      }
-    });
-    (0, _defineProperty2.default)((0, _assertThisInitialized2.default)(_this), "updateSliderValues", function () {
-      if (typeof _this.props.min !== "undefined" && (typeof _this.mySlider.min !== "undefined" || typeof _this.mySlider.options.min !== "undefined")) {
-        _this.mySlider.setAttribute("min", _this.props.min);
-      }
-
-      if (typeof _this.props.max !== "undefined" && (typeof _this.mySlider.max !== "undefined" || typeof _this.mySlider.options.max !== "undefined")) {
-        _this.mySlider.setAttribute("max", _this.props.max);
-      }
-
-      if (typeof _this.props.step !== "undefined" && (typeof _this.mySlider.step !== "undefined" || typeof _this.mySlider.options.step !== "undefined")) {
-        _this.mySlider.setAttribute("step", _this.props.step);
-      }
-
-      _this.mySlider.setValue(_this.props.value);
-
-      _this.checkAndDoDisabled();
-    });
-    return _this;
-  }
-
-  (0, _createClass2.default)(ReactBootstrapSlider, [{
-    key: "componentDidMount",
-    value: function componentDidMount() {
-      var that = this;
-      var sliderAttributes = (0, _objectSpread2.default)({}, this.props, {
+    componentDidMount() {
+      const that = this;
+      const sliderAttributes = { ...this.props,
         tooltip: this.props.tooltip || "show"
-      }); // console.log("sliderAttributes = " + JSON.stringify(sliderAttributes, null, 4));
+      }; // console.log("sliderAttributes = " + JSON.stringify(sliderAttributes, null, 4));
 
       this.mySlider = new _bootstrapSlider.default(this.node, sliderAttributes); //     this.updateSliderValues();
 
       if (this.props.change || this.props.handleChange) {
-        var changeEvent = this.props.change || this.props.handleChange;
-        this.mySlider.on("change", function (e) {
-          var fakeEvent = {
+        const changeEvent = this.props.change || this.props.handleChange;
+        this.mySlider.on("change", e => {
+          const fakeEvent = {
             target: {
               value: e.newValue
             }
@@ -103,8 +83,8 @@ function (_React$Component) {
       }
 
       if (this.props.slideStop) {
-        this.mySlider.on("slideStop", function (e) {
-          var fakeEvent = {
+        this.mySlider.on("slideStop", e => {
+          const fakeEvent = {
             target: {
               value: e
             }
@@ -115,33 +95,38 @@ function (_React$Component) {
 
       this.checkAndDoDisabled();
     }
-  }, {
-    key: "componentDidUpdate",
-    value: function componentDidUpdate() {
+
+    componentDidUpdate() {
       this.updateSliderValues();
     }
-  }, {
-    key: "componentWillUnmount",
-    value: function componentWillUnmount() {
+
+    componentWillUnmount() {
       this.mySlider.destroy();
     }
-  }, {
-    key: "render",
-    value: function render() {
-      var _this2 = this;
 
+    render() {
       // The slider"s an input.  That"s all we need.  We"ll do the rest in
       // the componentDidMount() method.
       return _react.default.createElement("div", {
-        ref: function ref(node) {
-          return _this2.node = node;
-        }
+        ref: node => this.node = node
       });
     }
-  }]);
-  return ReactBootstrapSlider;
-}(_react.default.Component);
 
-exports.ReactBootstrapSlider = ReactBootstrapSlider;
-var _default = ReactBootstrapSlider;
-exports.default = _default;
+  }
+
+  _exports.ReactBootstrapSlider = ReactBootstrapSlider;
+  ReactBootstrapSlider.propTypes = {
+    min: _propTypes.default.number,
+    max: _propTypes.default.number,
+    step: _propTypes.default.number,
+    value: _propTypes.default.oneOfType([_propTypes.default.number, _propTypes.default.arrayOf(_propTypes.default.number.isRequired).isRequired]).isRequired,
+    disabled: _propTypes.default.string,
+    tooltip: _propTypes.default.string,
+    change: _propTypes.default.func,
+    handleChange: _propTypes.default.func,
+    slideStop: _propTypes.default.func,
+    labelledby: _propTypes.default.oneOfType([_propTypes.default.string, _propTypes.default.arrayOf(_propTypes.default.string)])
+  };
+  var _default = ReactBootstrapSlider;
+  _exports.default = _default;
+});
